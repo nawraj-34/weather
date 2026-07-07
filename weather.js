9179b071d0360cba3113ea5cab239d29eebb9473
nawraj-34 <chaudharynawraj929@gmail.com>
2026-07-07 20:57:41 +0545

    changes with day

--- weather.js
+++ weather.js
@@ -21,25 +21,25 @@ async function FetchResult(location) {
     temperature_C.innerText = temperature;
     region_name.innerText = region;
     switch(day){
-        case 1:
+        case 0:
             day_name.innerText = "sunday";
             break;
-        case 2:
+        case 1:
             day_name.innerText = "Monday";
             break;
-        case 3:
+        case 2:
             day_name.innerText = "Tuesday";
             break;
-        case 4:
+        case 3:
             day_name.innerText ="Wednesday";
             break;
-        case 5:
+        case 4:
             day_name.innerText ="Thusday";
             break;
-        case 6:
+        case 5:
             day_name.innerText ="Friday";
             break;
-        case 7:
+        case 6:
             day_name.innerText="Saturday";
     }
     date_current.innerText = date;
