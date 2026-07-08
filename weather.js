3225a7f60860f72a3b4acdae0056c6d633603a61
nawraj-34 <chaudharynawraj929@gmail.com>
2026-07-08 21:51:00 +0545

    addding script of dropdown

--- weather.js
+++ weather.js
@@ -13,33 +13,34 @@ async function FetchResult(location) {
     let url = `https://api.weatherapi.com/v1/current.json?key=c1048f91529442018aa71308260607&q=${location}&aqi=no`
     let result = await fetch(url);
     let data = await result.json();
-    let temperature = data.current.temp_c;
+    let temperature = data.current.temp_c,°C;
     let region = location;
+    console.log(data);
     let day = data.current.is_day;
     let date = data.location.localtime;
     let condition = data.current.condition.text;
     temperature_C.innerText = temperature;
     region_name.innerText = region;
     switch(day){
-        case 2:
+        case 5:
             day_name.innerText = "sunday";
             break;
-        case 3:
+        case 6:
             day_name.innerText = "Monday";
             break;
-        case 4:
+        case 0:
             day_name.innerText = "Tuesday";
             break;
-        case 5:
+        case 1:
             day_name.innerText ="Wednesday";
             break;
-        case 6:
+        case 2:
             day_name.innerText ="Thusday";
             break;
-        case 7:
+        case 3:
             day_name.innerText ="Friday";
             break;
-        case 8:
+        case 4:
             day_name.innerText="Saturday";
     }
     date_current.innerText = date;
